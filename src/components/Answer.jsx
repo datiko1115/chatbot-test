64cca3ffac08bb0c3dffca8a0c3d0bde09873507
datiko1115 <datiko1115@gmail.com>
2022-05-05 21:43:44 +0900

    ボタン作成

---
+++ src/components/Answer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+// マテリアルUIのスタイルを決める記述
+const useStyles = makeStyles((theme) => ({
+  root: {
+
+  },
+}));
+// マテリアルUIのスタイルを決める記述
+
+
+const Answer = (props) => {
+    // マテリアルUIを埋め込む記述
+    // const classes = useStyles();  
+    // マテリアルUIを埋め込む記述
+
+    return (
+      // マテリアルUIを埋め込む記述button
+      <Button variant="contained" color="primary">
+        {props.content}
+      </Button>  
+      // マテリアルUIを埋め込む記述button
+    )
+}
+
+export default Answer
---
+++ src/components/AnswersList.jsx
@@ -0,0 +1,18 @@
+import { VideoLabelRounded } from '@material-ui/icons'
+import React from 'react'
+import {Answer} from './index'
+// ↑エントリポイントから呼び出している/indexディレクトリの{Answer}↑
+
+
+const AnswersList = (props) => {
+  return(
+    <div className="c-grid__answer">
+      {props.answers.map((value, index) => {
+        return <Answer content={value.content} key={index.toString()} />
+        // Answerから受け取ったcontentのpropsを取得mapメソッドで繰り返し処理で一覧で表示させる処理
+      })}
+    </div>
+  )
+}
+
+export default AnswersList
